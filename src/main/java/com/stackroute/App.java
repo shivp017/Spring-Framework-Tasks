0bbe8bca6f314dc81a1cc366908636f1b6769b17
shiv.prakash <shiv.prakash@cgi.com>
2019-07-26 17:38:02 +0530

    updated task 1 spring fundamentals

---
+++ src/main/java/com/stackroute/App.java
@@ -0,0 +1,26 @@
+package com.stackroute;
+
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie=context.getBean("movie",Movie.class);
+        System.out.println(movie.getActor());
+
+        XmlBeanFactory value=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Movie movie1=context.getBean("movie",Movie.class);
+        System.out.println(movie1.getActor());
+
+    }
+}
